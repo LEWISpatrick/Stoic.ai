41b23a5cefbbfbe677e248b5f1d6b7aaf5b7a086
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2023-07-12 11:55:00 -0300

    Add files via upload

--- stoic_ai.py
+++ stoic_ai.py
@@ -1,29 +1,24 @@
-#!/usr/bin/env python3
-
-"A program to fetch stoic quotations and display them"
-
-import random
-import os
 import tkinter as tk
 from PIL import ImageTk, Image
+import random
 import requests
 from bs4 import BeautifulSoup
+import os
+from gtts import gTTS
+import pygame
 
+stoic_website_url = "https://iep.utm.edu/stoicism/"
 # The URL of the Stoic website from which quotes will be fetched
-STOIC_WEBSITE_URL = "https://iep.utm.edu/stoicism/"
 
+image_url = "https://dailystoic.com/wp-content/uploads/2016/07/marcusaureliusdailystoic-scaled.jpg"
 # The URL of the image to be displayed alongside the Stoic quote
-IMAGE_URL = "https://dailystoic.com/wp-content/uploads/2016/07/marcusaureliusdailystoic-scaled.jpg"
 
-# The local path where the downloaded image will be saved
 image_path = os.path.join(os.getcwd(), "download (2)")
-
+# The local path where the downloaded image will be saved
 
 def get_stoic_quotes():
-    "Fetch quotations from the web and extract them to a list"
-
     # Send a GET request to the Stoic website
-    response = requests.get(STOIC_WEBSITE_URL)
+    response = requests.get(stoic_website_url)
 
     if response.status_code == 200:
         # Parse the HTML content of the response using BeautifulSoup
@@ -40,34 +35,55 @@ def get_stoic_quotes():
 
         # Return the list of Stoic quotes
         return quotes
-
-    # If the request fails, print an error message and return an empty list
-    print("Failed to retrieve Stoic quotes. Status code:", response.status_code)
-    return []
-
+    else:
+        # If the request fails, print an error message and return an empty list
+        print("Failed to retrieve Stoic quotes. Status code:", response.status_code)
+        return []
 
 def download_image():
-    "Download the image"
     # Download the image from the given URL
-    response = requests.get(IMAGE_URL, stream=True)
+    response = requests.get(image_url, stream=True)
     if response.status_code == 200:
         # If the download is successful, save the image to the specified path
         with open(image_path, "wb") as file:
             file.write(response.content)
         return True
+    else:
+        # If the download fails, print an error message with the status code
+        print("Failed to download image. Status code:", response.status_code)
+        return False
 
-    # If the download fails, print an error message with the status code
-    print("Failed to download image. Status code:", response.status_code)
-    return False
+def text_to_speech(text, filename):
+    # Convert the given text to speech and save it as an audio file
+    tts = gTTS(text=text, lang='en')
+    tts.save(filename)
 
+def play_audio(filename):
+    # Play the audio file
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
 
 def display_stoic_popup():
-    "Display the image"
     quotes = get_stoic_quotes()  # Fetch Stoic quotes
     if quotes:
         quote = random.choice(quotes)  # Select a random quote from the retrieved quotes
     else:
         quote = "No Stoic quotes found."
+
+    def update_quote():
+        # Update the displayed quote with a new random quote
+        new_quote = random.choice(quotes)
+        quote_text.configure(state="normal")
+        quote_text.delete("1.0", tk.END)
+        quote_text.insert(tk.END, new_quote)
+        quote_text.configure(state="disabled")
+
+    def read_out_loud():
+        # Convert the quote to speech and play it
+        filename = "quote_audio.mp3"
+        text_to_speech(quote, filename)
+        play_audio(filename)
 
     root = tk.Tk()
     root.title("Stoic Quote")
@@ -76,36 +92,35 @@ def display_stoic_popup():
     if download_image():
         # If the image download is successful, open and display the image
         image = Image.open(image_path)
-        image = image.resize((500, 500), Image.ANTIALIAS)
+        image = image.resize((500, 500), Image.LANCZOS)
 
-        # Create an empty label widget for displaying an image
         image_label = tk.Label(root)
-
-        # Pack the image label widget into the main window with some vertical padding
         image_label.pack(pady=20)
 
-        # Create a label widget for displaying a quote with specified font and wrap length
-        quote_label = tk.Label(root, text=quote, font=("Serif", 16), wraplength=600)
+        button_frame = tk.Frame(root)
+        button_frame.pack()
 
-        # Pack the quote label widget into the main window with some vertical padding
-        quote_label.pack(pady=20)
+        read_button = tk.Button(button_frame, text="Read Out Loud", command=read_out_loud)
+        read_button.pack(side=tk.LEFT, padx=5, pady=10)
 
-        def update_quote():
-            # Update the displayed quote with a new random quote
-            quote_label.config(text=random.choice(quotes))
-            root.after(60000, update_quote)  # Update the quote every 60 seconds
+        make_another_button = tk.Button(button_frame, text="Make Another Quote", command=update_quote)
+        make_another_button.pack(side=tk.LEFT, padx=5, pady=10)
 
-        update_quote()
+        quote_frame = tk.Frame(root)
+        quote_frame.pack(pady=20)
 
-        # Create a Tkinter-compatible photo image from the loaded image
+        quote_text = tk.Text(quote_frame, wrap="word", font=("Serif", 16))
+        quote_text.pack(side=tk.LEFT, fill=tk.Y)
+
+        quote_scroll = tk.Scrollbar(quote_frame, command=quote_text.yview)
+        quote_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        quote_text.configure(yscrollcommand=quote_scroll.set)
+        quote_text.insert(tk.END, quote)
+        quote_text.configure(state="disabled")
+
         image_tk = ImageTk.PhotoImage(image)
-
-        # Configure the image label widget to display the photo image
         image_label.config(image=image_tk)
-
-        # Store a reference to the photo image in the image label
-        # widget to prevent it from being garbage collected
-
         image_label.image = image_tk
 
     else:
@@ -113,8 +128,19 @@ def display_stoic_popup():
         quote_label = tk.Label(root, text=quote, font=("Serif", 16), wraplength=600)
         quote_label.pack(pady=20)
 
+        button_frame = tk.Frame(root)
+        button_frame.pack()
+
+        read_button = tk.Button(button_frame, text="Read Out Loud", command=read_out_loud)
+        read_button.pack(side=tk.LEFT, padx=5, pady=10)
+
+        make_another_button = tk.Button(button_frame, text="Make Another Quote", command=update_quote)
+        make_another_button.pack(side=tk.LEFT, padx=5, pady=10)
+
     root.mainloop()
 
+# Initialize the pygame mixer
+pygame.mixer.init()
 
 # Display Stoic quote pop-up
 display_stoic_popup()
